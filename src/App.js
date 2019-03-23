fc446edbbc04a040e2c10184c40cd076bb89e200
adolfo2 <48386860+adolfo2@users.noreply.github.com>
2019-03-23 16:33:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Welcome to the ASM devops tools course. I will see you all Next Saturday.
         </p>
       </div>
     );
